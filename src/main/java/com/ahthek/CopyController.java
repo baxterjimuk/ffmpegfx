9c04d3f04840d6af460630317647f39d33859d55
Baxter Jimuk <baxterjimuk@gmail.com>
2026-02-25 13:50:22 +0800

    bat file for robo-copy-move done. haven't really tested with real file yet each created bat file.

--- src/main/java/com/ahthek/CopyController.java
+++ src/main/java/com/ahthek/CopyController.java
@@ -1,12 +1,18 @@
 package com.ahthek;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.prefs.Preferences;
 
 import javafx.beans.binding.Bindings;
@@ -18,17 +24,23 @@ import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.HBox;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
 public class CopyController {
@@ -52,7 +64,7 @@ public class CopyController {
   private Label sizeLabel;
 
   @FXML
-  private CheckBox shutdownCheckBox;
+  private CheckBox shutdownCheckBox, directoryCheckBox;
 
   @FXML
   private HBox timerHBox;
@@ -118,8 +130,72 @@ public class CopyController {
   }
 
   @FXML
-  private void createBat() {
-    System.out.println(((RadioButton) actionGroup.getSelectedToggle()).getText());
+  private void createBat(ActionEvent event) {
+    String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+    String action = ((RadioButton) actionGroup.getSelectedToggle()).getText();
+    String destination = destinationTextField.getText();
+
+    Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+    stage.getIcons().add(new Image("clapfx.png"));
+    FileChooser fileChooser = new FileChooser();
+    fileChooser.setTitle("Save File");
+    fileChooser.getExtensionFilters().addAll(new ExtensionFilter("All Files", "*.*"));
+    fileChooser.setInitialFileName(now + "_" + action + ".bat");
+    File initialDir = new File(preferences.get("lastUsedDir", System.getProperty("user.home")));
+    if (!initialDir.exists()) {
+      initialDir = new File(System.getProperty("user.home"));
+    }
+    fileChooser.setInitialDirectory(initialDir);
+    File file = fileChooser.showSaveDialog(stage);
+    if (file != null) {
+      Alert alert = new Alert(AlertType.CONFIRMATION);
+      alert.setTitle("FFMPEGFX");
+      alert.setHeaderText("Create batch file");
+      alert.setContentText("\"" + file.getName() + "\" will be created in\n" 
+        + "\"" + file.getParent() 
+        + "\"\n\nAre you sure you want to continue?"
+      );
+      Optional<ButtonType> result = alert.showAndWait();
+      if (result.isPresent() && result.get() == ButtonType.OK) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(file.getAbsolutePath(), true))) {
+          writer.println("@chcp 65001");
+          writer.println("@echo [%date% %time%] Process started. "
+            + sizeLabel.getText().replaceAll("[()]", "") + " to be copied."
+          );
+          for (Path path : sourcePaths) {
+            if (action.equals("Robo-Copy")) {
+              robocopy(path, writer, destination);
+            } else {
+              robomove(path, writer, destination);
+            }
+          }
+          if (shutdownCheckBox.isSelected()) {
+            writer.println("@shutdown -s -t " + durationInSeconds());
+            Duration duration = Duration.ofSeconds(durationInSeconds());
+            writer.println("@echo [%date% %time%] Process completed! Auto-shutdown in ~ "
+              + String.valueOf(duration.toDaysPart()) + " days "
+              + String.valueOf(duration.toHoursPart()) + " hours "
+              + String.valueOf(duration.toMinutesPart()) + " minutes "
+              + String.valueOf(duration.toSecondsPart()) + " seconds."
+            );
+          }
+          writer.println("@pause");
+          writer.close();
+          alert.setAlertType(AlertType.INFORMATION);
+          TextArea textArea = new TextArea("Process completed!\n\n\""
+            + file.getName() + "\" can be found at\n\"" + file.getParent() + "\""
+          );
+          textArea.setEditable(false);
+          textArea.setWrapText(true);
+          textArea.setMaxWidth(Double.MAX_VALUE);
+          textArea.setMaxHeight(Double.MAX_VALUE);
+          alert.getDialogPane().setContent(textArea);
+          alert.showAndWait();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }
   }
 
   @FXML
@@ -258,5 +334,39 @@ public class CopyController {
     hSpinner.getValueFactory().setValue(0);
     mSpinner.getValueFactory().setValue(0);
     sSpinner.getValueFactory().setValue(0);
+  }
+
+  private void robocopy(Path path, PrintWriter writer, String destination) {
+    if (Files.isDirectory(path)) {
+      if (directoryCheckBox.isSelected()) {
+        writer.println("robocopy \"" + path.toString() + "\" \"" 
+          + Paths.get(destination, path.getFileName().toString()).toString() 
+          + "\" /E"
+        );
+      } else {
+        writer.println("robocopy \"" + path.toString() + "\" \"" + destination + "\" /E");
+      }
+    } else {
+      writer.println("robocopy \"" + path.getParent().toString() + "\" \""
+        + destination + "\" \"" + path.getFileName().toString() + "\""
+      );
+    }
+  }
+
+  private void robomove(Path path, PrintWriter writer, String destination) {
+    if (Files.isDirectory(path)) {
+      if (directoryCheckBox.isSelected()) {
+        writer.println("robocopy \"" + path.toString() + "\" \"" 
+          + Paths.get(destination, path.getFileName().toString()).toString() 
+          + "\" /MOVE /E"
+        );
+      } else {
+        writer.println("robocopy \"" + path.toString() + "\" \"" + destination + "\" /MOVE /E");
+      }
+    } else {
+      writer.println("robocopy \"" + path.getParent().toString() + "\" \""
+        + destination + "\" \"" + path.getFileName().toString() + "\" /MOV"
+      );
+    }
   }
 }
